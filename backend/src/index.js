d1adcf21c37f370c54f8aa819655a2923cf57cc4
Jacobo Uribe <jacouribe2003@gmail.com>
2022-05-25 15:28:54 -0500

    mount first part of proyect

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(3003, () => {
+  console.log(`Listening on port ${3003}`)
+})
